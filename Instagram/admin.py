95895bf0ca591be15438d4554cea7bb7c4972834
He Li <heli@bu.edu>
2020-01-01 00:25:28 -0500

    layout with bootstrap

--- Instagram/admin.py
+++ Instagram/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from Instagram.models import Post, InstaUser
+from Instagram.models import Post, InstaUser, Like
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(InstaUser)
+admin.site.register(Like)
--- Instagram/models.py
+++ Instagram/models.py
@@ -4,21 +4,6 @@ from imagekit.models import ProcessedImageField
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
-class Post(models.Model):
-    title = models.TextField(blank=True, null=True)
-    image = ProcessedImageField(
-        upload_to = 'static/images/posts',
-        format='JPEG',
-        options={'quality': 100},
-        blank=True,
-        null=True,
-    )
-
-    def get_absolute_url(self):
-            return reverse("post_detail", args=[str(self.id)])
-
-
 class InstaUser(AbstractUser):
     profile_pic = ProcessedImageField(
         upload_to = 'static/images/profiles',
@@ -26,4 +11,65 @@ class InstaUser(AbstractUser):
         options={'quality': 100},
         blank=True,
         null=True,
-    )
+        )
+
+    def __str__(self):
+        return self.username
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        InstaUser, 
+        on_delete=models.CASCADE, 
+        related_name="my_posts"
+        )
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        upload_to = 'static/images/posts',
+        format='JPEG',
+        options={'quality': 100},
+        blank=True,
+        null=True,
+        )
+
+    posted_on = models.DateTimeField(
+        auto_now_add=True, 
+        editable=False
+        )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+            return reverse("post_detail", args=[str(self.id)])
+    
+    def get_like_count(self):
+        return self.likes.count()
+
+    def get_comment_count(self):
+        return self.comments.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE, 
+        related_name='comments',
+        )
+    user = models.ForeignKey(
+        InstaUser, 
+        on_delete=models.CASCADE
+        )
+    comment = models.CharField(max_length=100)
+    posted_on = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.comment
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes',)
+    user = models.ForeignKey(InstaUser, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("post", "user")
+
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' ' + self.post.title
